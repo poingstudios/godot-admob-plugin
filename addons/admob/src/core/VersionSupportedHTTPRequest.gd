150edfeeeeb6b1b734539a7a1788448e04a6e96d
Gustavo Maciel <gustamaciel2012@hotmail.com>
2023-08-14 23:21:31 -0300

    feat: added new to get version supported

--- addons/admob/src/core/VersionSupportedHTTPRequest.gd
+++ addons/admob/src/core/VersionSupportedHTTPRequest.gd
@@ -6,11 +6,11 @@ signal supported_version_changed(value_dictionary)
 var AdMobGlobals = preload("res://addons/admob/src/utils/AdMobGlobals.gd")
 
 func _ready():
-	request("https://gist.githubusercontent.com/gumaciel/ba15bd85ebcc87866c24a819de37cc9a/raw/admob_editor_versions_supported.json")
+	request("https://raw.githubusercontent.com/Poing-Studios/godot-admob-versions/" + AdMobGlobals.get_plugin_version() + "/versions.json")
 
 
 func _on_VersionSupportedHTTPRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
 	
-	var version_support : Dictionary = json.result[AdMobGlobals.get_plugin_version()]
+	var version_support : Dictionary = json.result
 	emit_signal("supported_version_changed", version_support)
