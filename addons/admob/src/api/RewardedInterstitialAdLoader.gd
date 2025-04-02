b770a1ae690d8532235313627a963e47b830954c
Youngmin Koo <youngminz.kr@gmail.com>
2025-04-02 18:19:38 +0900

    refactor: change plugin signals from lambdas to member functions

--- addons/admob/src/api/RewardedInterstitialAdLoader.gd
+++ addons/admob/src/api/RewardedInterstitialAdLoader.gd
@@ -40,12 +40,17 @@ func load(
 	rewarded_ad_load_callback := RewardedInterstitialAdLoadCallback.new()) -> void:
 
 	if _plugin:
+		self.ad_unit_id = ad_unit_id
+		self.ad_request = ad_request
+		self.rewarded_ad_load_callback = rewarded_ad_load_callback
 		_plugin.load(ad_unit_id, ad_request.convert_to_dictionary(), ad_request.keywords, _uid)
-		_plugin.connect("on_rewarded_interstitial_ad_loaded", func(uid : int):
-			if uid == _uid:
-				rewarded_ad_load_callback.on_ad_loaded.call_deferred(RewardedInterstitialAd.new(uid))
-		, CONNECT_ONE_SHOT)
-		_plugin.connect("on_rewarded_interstitial_ad_failed_to_load", func(uid : int, load_ad_error_dictionary : Dictionary): 
-			if uid == _uid:
-				rewarded_ad_load_callback.on_ad_failed_to_load.call_deferred(LoadAdError.create(load_ad_error_dictionary))
-		, CONNECT_ONE_SHOT)
+		_plugin.connect("on_rewarded_interstitial_ad_loaded", _on_rewarded_interstitial_ad_loaded, CONNECT_ONE_SHOT)
+		_plugin.connect("on_rewarded_interstitial_ad_failed_to_load", _on_rewarded_interstitial_ad_failed_to_load, CONNECT_ONE_SHOT)
+
+func _on_rewarded_interstitial_ad_loaded(uid : int) -> void:
+	if uid == _uid:
+		rewarded_ad_load_callback.on_ad_loaded.call_deferred(RewardedInterstitialAd.new(uid))
+
+func _on_rewarded_interstitial_ad_failed_to_load(uid : int, load_ad_error_dictionary : Dictionary) -> void: 
+	if uid == _uid:
+		rewarded_ad_load_callback.on_ad_failed_to_load.call_deferred(LoadAdError.create(load_ad_error_dictionary))
--- addons/admob/src/ump/api/ConsentForm.gd
+++ addons/admob/src/ump/api/ConsentForm.gd
@@ -30,13 +30,16 @@ var _uid : int
 func _init(UID : int):
 	self._uid = UID
 
+var _on_consent_form_dismissed_callback
+
 func show(on_consent_form_dismissed := func(form_error : FormError) : pass) -> void:
 	if _plugin:
+		self._on_consent_form_dismissed_callback = on_consent_form_dismissed
 		_plugin.show(_uid)
+		_plugin.connect("on_consent_form_dismissed", _on_consent_form_dismissed, CONNECT_ONE_SHOT)
 
-		_plugin.connect("on_consent_form_dismissed", func(uid : int, form_error_dictionary : Dictionary) :
-			if uid == _uid:
-				var formError : FormError = FormError.create(form_error_dictionary) if not form_error_dictionary.is_empty() else null
-				on_consent_form_dismissed.call_deferred(formError)
-			, CONNECT_ONE_SHOT)
+func _on_consent_form_dismissed(uid : int, form_error_dictionary : Dictionary) -> void:
+	if uid == _uid:
+		var formError : FormError = FormError.create(form_error_dictionary) if not form_error_dictionary.is_empty() else null
+		_on_consent_form_dismissed_callback.call_deferred(formError)
 
