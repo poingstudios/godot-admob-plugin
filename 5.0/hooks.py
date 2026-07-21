53b6711f58448b9e53dff3de29807596df57af60
Gustavo Maciel <gustamaciel2012@hotmail.com>
2026-07-21 08:14:59 -0300

    Deployed a2d7a243 to 5.0 with MkDocs 1.6.1 and mike 2.1.3

--- 5.0/hooks.py
+++ 5.0/hooks.py
@@ -1,7 +1,11 @@
 import datetime
 
 def on_config(config, **kwargs):
+    current_year = str(datetime.date.today().year)
     if 'copyright' in config:
-        current_year = str(datetime.date.today().year)
         config['copyright'] = config['copyright'].replace('{current_year}', current_year)
+    if 'extra' not in config:
+        config['extra'] = {}
+    config['extra']['current_year'] = current_year
     return config
+
