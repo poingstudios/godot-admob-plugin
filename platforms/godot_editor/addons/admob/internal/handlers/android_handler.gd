dc73dc8a8da59b3129fff272441047cc0d2fe20c
Gustavo Maciel <gustamaciel2012@hotmail.com>
2026-07-20 22:20:41 -0300

    fix: remove 'v' prefix from template zip file names for consistency

--- platforms/godot_editor/addons/admob/internal/handlers/android_handler.gd
+++ platforms/godot_editor/addons/admob/internal/handlers/android_handler.gd
@@ -76,4 +76,4 @@ func _on_download_completed(success: bool) -> void:
 
 
 static func get_zip_file_name() -> String:
-	return "android-template-v" + PluginVersion.godot + ".zip"
+	return "android-template-" + PluginVersion.godot + ".zip"
